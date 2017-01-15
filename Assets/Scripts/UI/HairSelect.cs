1129fa018e421aae4b3e9b01e85af0f27b782397
moobin2 <anqls20000@naver.com>
2017-01-16 02:38:27 +0900

    커스터마이징씬
    헤어 셀렉트 추가 좀더 추가해야함

---
+++ Assets/Scripts/UI/HairSelect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HairSelect : MonoBehaviour {
+
+    private GameObject[]    UI_HairObject;
+    private int             UI_HairNumber = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        UI_HairObject = Resources.LoadAll<GameObject>("ManHair");
+
+        Debug.Log(UI_HairObject.Length);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Instantiate(UI_HairObject[UI_HairNumber], Vector3.zero, Quaternion.identity);
+	}
+}
