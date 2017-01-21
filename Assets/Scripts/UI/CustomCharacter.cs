774b5a594f42b5dadfe95f97c4b613597ce742fa
moobin2 <anqls20000@naver.com>
2017-01-21 23:44:54 +0900

    케릭터선택씬 추가 및 커스터마이징씬 완
    씬체인지는 아직 미추가

---
+++ Assets/Scripts/UI/CustomCharacter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomCharacter : MonoBehaviour {
+
+    private GameObject BaseManCharacter;
+    private GameObject BaseWomanCharacter;
+    private int Character_Hair_Number = 0;
+    private int Character_Cloth_Number = 0;
+
+    // Use this for initialization
+    void Start ()
+    {
+        BaseManCharacter = this.transform.FindChild("BaseMan").gameObject;
+        BaseManCharacter.SetActive(true);
+
+        BaseWomanCharacter = this.transform.FindChild("BaseWoman").gameObject;
+        BaseWomanCharacter.SetActive(false);
+
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.A))
+        {
+            BaseManCharacter.SetActive(true);
+            BaseWomanCharacter.SetActive(false);
+        }
+        else if(Input.GetKeyDown(KeyCode.D))
+        {
+            BaseManCharacter.SetActive(false);
+            BaseWomanCharacter.SetActive(true);
+        }
+	}
+}
---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public float            UI_ObjectRotSpeed = 100f;
+
+    public GameObject       Character;
+    public GameObject       HairPosition;
+
+    private GameObject[]    UI_HairResources;
+    private GameObject      UI_HairObject;
+
+    private GameObject      Char_HairObject;
+
+    private int UI_HairNumber = 7;
+    private bool IsMan = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        UI_HairResources = Resources.LoadAll<GameObject>("Hair");
+
+        CreateUIHairObject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UI_HairObject.transform.Rotate(Vector3.up * Time.deltaTime * UI_ObjectRotSpeed, Space.World);
+    }
+
+    void CreateUIHairObject()
+    {
+        if (UI_HairObject != null)
+        {
+            Destroy(UI_HairObject);
+        }
+
+        UI_HairObject = Instantiate(UI_HairResources[UI_HairNumber]) as GameObject;
+        UI_HairObject.transform.localScale = Vector3.one * 0.4f;
+        UI_HairObject.transform.parent = transform.FindChild("Hair").transform;
+        UI_HairObject.transform.localPosition = Vector3.zero;
+        UI_HairObject.layer = 5;
+
+
+        CharacterAddObject(HairPosition, UI_HairObject);
+
+    }
+
+    public void SelectMan(Material FirstMaterial)
+    {
+        if (IsMan == true) return;
+
+        IsMan = true;
+        UI_HairNumber = 7;
+        Character.transform.FindChild("Base").GetComponent<SkinnedMeshRenderer>().material = FirstMaterial;
+
+        CreateUIHairObject();
+    }
+
+    public void SelectWoman(Material FirstMaterial)
+    {
+        if (IsMan == false) return;
+
+        IsMan = false;
+        UI_HairNumber = 0;
+        Character.transform.FindChild("Base").GetComponent<SkinnedMeshRenderer>().material = FirstMaterial;
+
+        CreateUIHairObject();
+    }
+
+    public void IncreaseHairNumber()
+    {
+        UI_HairNumber++;
+
+        if(IsMan == true && UI_HairNumber >= UI_HairResources.Length)
+        {
+            UI_HairNumber = 7;
+        }
+        else if(IsMan == false && UI_HairNumber >= 7)
+        {
+            UI_HairNumber = 0;
+        }
+        CreateUIHairObject();
+    }
+
+    public void DecreaseHairNumber()
+    {
+        UI_HairNumber--;
+
+        if (IsMan == true && UI_HairNumber < 7)
+        {
+            UI_HairNumber = UI_HairResources.Length - 1;
+        }
+        else if (IsMan == false && UI_HairNumber < 0)
+        {
+            UI_HairNumber = 6;
+        }
+        CreateUIHairObject();
+    }
+
+    void CharacterAddObject(GameObject Position, GameObject AddObject)
+    {
+        if (Char_HairObject != null)
+        {
+            Destroy(Char_HairObject);
+        }
+
+        Char_HairObject = Instantiate(AddObject) as GameObject;
+        Char_HairObject.transform.localScale = Vector3.one;
+        Char_HairObject.transform.parent = Position.transform;
+        Char_HairObject.transform.localPosition = Vector3.zero;
+        Char_HairObject.transform.localRotation = Quaternion.Euler(-90.0f, 0f, 0f);
+        Char_HairObject.layer = 0;
+    }
+
+    public void ChangeClothMaterial(Material ManClothes, Material WomanClothes)
+    {
+        if (IsMan == true)
+        {
+            Character.transform.FindChild("Base").GetComponent<SkinnedMeshRenderer>().material = ManClothes;
+        }
+        else
+        {
+            Character.transform.FindChild("Base").GetComponent<SkinnedMeshRenderer>().material = WomanClothes;
+        }
+    }
+}
